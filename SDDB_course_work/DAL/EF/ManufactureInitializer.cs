48aa58dc0554ef0bf6c14f37e4ea522b6c91432b
helenkramar <elena.kramar@hotmail.com>
2018-06-11 15:11:20 +0300

    feeling Manufacture db

--- SDDB_course_work/DAL/EF/ManufactureInitializer.cs
+++ SDDB_course_work/DAL/EF/ManufactureInitializer.cs
@@ -1,12 +1,19 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using DAL.Entities;
 
 namespace DAL.EF
 {
     public class ManufactureInitializer : DropCreateDatabaseAlways<ManufactureContext>
     {
-        protected override void Seed(ManufactureContext mDB)
+        protected override void Seed(ManufactureContext db)
         {
+            Position first = new Position {Name = "cake", Amount = 4};
+            Position second = new Position { Name = "cookie", Amount = 14 };
+            Position third = new Position { Name = "candy", Amount = 40 };
 
+            List<Position> listPosition = new List<Position> { first, second, third};
+            db.Positions.AddRange(listPosition);
         }
     }
 }
