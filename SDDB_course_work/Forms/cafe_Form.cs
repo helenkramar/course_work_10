d24bb7fa25a44e0d5948abe8f2b08bf3361569c5
helenkramar <elena.kramar@hotmail.com>
2018-06-16 00:56:28 +0300

    cafe grid

--- SDDB_course_work/Forms/cafe_Form.cs
+++ SDDB_course_work/Forms/cafe_Form.cs
@@ -1,17 +1,22 @@
 ﻿using System.Windows.Forms;
+using DAL.EF;
 
 namespace Forms
 {
     public partial class cafe_Form : Form
     {
+        private CafeContext cafeContext;
+
         public cafe_Form()
         {
-            InitializeComponent();
+           InitializeComponent();
+           cafeContext = new CafeContext("CafeContext");
         }
 
         private void cafe_Form_Load(object sender, System.EventArgs e)
         {
-
+            var data = cafeContext.Positions;
+            cafe_dataGrid.DataSource = data;
         }
 
         private void sell_button_Click(object sender, System.EventArgs e)
